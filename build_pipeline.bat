188f17c95c5b16ec5f1c7ac906ded9b61ae434bc
Laura Canalini <laura.canalini@unimore.it>
2019-10-28 15:35:06 +0100

    Add pipeline build script for Windows

---
+++ build_pipeline.bat
@@ -0,0 +1,43 @@
+set CUR_PATH=%cd:\=/%
+mkdir deephealth
+cd deephealth
+
+:: EDDL
+git clone git@github.com:deephealthproject/eddl.git --recursive
+cd eddl
+git checkout 9e11e447efb3184eaf8f747336eba9ce7fb86674
+mkdir bin
+cd bin
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TARGET=CPU -DEDDL_WITH_CUDA=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config Release --target INSTALL
+
+:: OPENCV
+cd %CUR_PATH%/deephealth/
+git clone git@github.com:opencv/opencv.git
+cd opencv
+git checkout tags/4.1.1
+mkdir build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_ZLIB=OFF -DBUILD_JPEG=ON -DBUILD_PNG=ON -DBUILD_TIFF=ON -DWITH_FFMPEG=OFF -DWITH_OPENCL=OFF -DWITH_QT=OFF -DWITH_IPP=OFF -DWITH_MATLAB=OFF -DWITH_OPENGL=OFF -DWITH_QT=OFF -DWITH_TIFF=OFF -DWITH_TBB=OFF -DWITH_V4L=OFF -DWITH_LAPACK=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF WITH_OPENEXR=OFF ..
+cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config Release --target INSTALL
+
+:: ECVL
+cd %CUR_PATH%/deephealth
+git clone git@github.com:deephealthproject/ecvl.git
+cd ecvl
+git checkout 4acb1f773ef3ed1c000aa4d2a25445d97314de64
+mkdir bin
+cd bin
+cmake -G "Visual Studio 15 2017 Win64" -DOpenCV_DIR=%CUR_PATH%/deephealth/opencv/build -DEDDL_DIR=%CUR_PATH%/deephealth/eddl/bin/install -DECVL_BUILD_EDDL=ON -DECVL_DATASET_PARSER=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config Release --target INSTALL
+
+:: PIPELINE
+cd %CUR_PATH%
+mkdir bin
+cd bin
+cmake -G "Visual Studio 15 2017 Win64" -DECVL_DIR=deephealth/ecvl/bin/install ..
+cmake --build . --config Release --target ALL_BUILD
+Release\\MNIST_BATCH.exe
--- build_pipeline.sh
+++ build_pipeline.sh
@@ -6,9 +6,8 @@ mkdir -p deephealth && cd deephealth
 ############ EDDL
 git clone git@github.com:deephealthproject/eddl.git --recursive
 cd eddl
-git checkout 9b4ac771c91e84495c5d0fbfb21edad6703cb339 # Develop branch
+git checkout 9e11e447efb3184eaf8f747336eba9ce7fb86674 # Master branch
 mkdir -p bin && cd bin
-#cmake -G "Visual Studio 15 2017 Win64"
 cmake -G "Unix Makefiles" -DBUILD_TARGET=GPU -DEDDL_WITH_CUDA=ON -DCMAKE_INSTALL_PREFIX=install ..
 make -j$(nproc) && make install
 
@@ -18,16 +17,16 @@ git clone git@github.com:opencv/opencv.git
 cd opencv
 git checkout tags/4.1.1
 mkdir -p build && cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_ZLIB=OFF -DWITH_FFMPEG=OFF -DWITH_OPENCL=OFF -DWITH_QT=OFF -DWITH_IPP=OFF -DWITH_MATLAB=OFF -DWITH_OPENGL=OFF -DWITH_QT=OFF -DWITH_TIFF=OFF -DWITH_TBB=OFF -DWITH_V4L=OFF -DWITH_LAPACK=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF WITH_OPENEXR=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_ZLIB=OFF -DBUILD_JPEG=ON -DBUILD_PNG=ON -DBUILD_TIFF=ON -DWITH_FFMPEG=OFF -DWITH_OPENCL=OFF -DWITH_QT=OFF -DWITH_IPP=OFF -DWITH_MATLAB=OFF -DWITH_OPENGL=OFF -DWITH_QT=OFF -DWITH_TIFF=OFF -DWITH_TBB=OFF -DWITH_V4L=OFF -DWITH_LAPACK=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF WITH_OPENEXR=OFF ..
 make -j$(nproc) && make install
 
 ############ ECVL
 cd $CUR_PATH/deephealth
 git clone git@github.com:deephealthproject/ecvl.git
 cd ecvl
-git checkout ce069064bb49442fc07ba34ed2f66dc8f1ababc1 # Master branch
+git checkout 4acb1f773ef3ed1c000aa4d2a25445d97314de64 # Master branch
 mkdir -p bin && cd bin
-cmake -G "Unix Makefiles" -DOpenCV_DIR=$CUR_PATH/deephealth/opencv/build -DEDDL_DIR=$CUR_PATH/deephealth/eddl/bin/install -DECVL_BUILD_EDDL=ON -DECVL_DATASET_PARSER=ON -DCMAKE_INSTALL_PREFIX=install ..
+cmake -G "Unix Makefiles" -DOpenCV_DIR=$CUR_PATH/deephealth/opencv/build -DEDDL_DIR=$CUR_PATH/deephealth/eddl/bin/install -DECVL_BUILD_EDDL=ON -DECVL_DATASET_PARSER=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
 make -j$(nproc) && make install
 
 ############ PIPELINE
