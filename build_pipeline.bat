213f0a49d0d9088da72371cf71e67b728011240b
Michele Cancilla <cancilla.michele@gmail.com>
2020-04-29 15:22:07 +0200

    Update build scripts

--- build_pipeline.bat
+++ build_pipeline.bat
@@ -1,43 +1,46 @@
-set CUR_PATH=%cd:\=/%
-mkdir deephealth
-cd deephealth
+@echo off
+set UCP_PATH=%cd:\=/%
+set DEVICE=GPU
+set BUILD_TYPE=Release
+REM set BUILD_TYPE=Debug
 
-:: EDDL
-git clone https://github.com/deephealthproject/eddl.git --recursive
+mkdir deephealth & cd deephealth
+
+REM EDDL
+git clone --recurse-submodule https://github.com/deephealthproject/eddl.git
 cd eddl
-git checkout tags/v0.4.2
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TARGET=GPU -DBUILD_SHARED_LIB=OFF -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX=install ..
-cmake --build . --config Release --target ALL_BUILD
-cmake --build . --config Release --target INSTALL
+git checkout tags/0.4.3
+mkdir build & cd build
+cmake -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DBUILD_TARGET=%DEVICE% -DBUILD_SHARED_LIB=OFF -DBUILD_PROTOBUF=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . --config %BUILD_TYPE% --target ALL_BUILD --parallel 4
+cmake --build . --config %BUILD_TYPE% --target INSTALL
+set EDDL_INSTALL_DIR=%UCP_PATH%/deephealth/eddl/build/cmake
 
-:: OPENCV
-cd %CUR_PATH%/deephealth/
+REM OPENCV
+cd %UCP_PATH%\deephealth
 git clone https://github.com/opencv/opencv.git
 cd opencv
-git checkout tags/4.1.1
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs,photo -DBUILD_opencv_apps=OFF -DBUILD_opencv_java_bindings_generator=OFF -DBUILD_opencv_python3=OFF -DBUILD_opencv_python_bindings_generator=OFF -DBUILD_opencv_python_tests=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_ZLIB=OFF -DBUILD_JPEG=ON -DBUILD_PNG=ON -DBUILD_TIFF=ON -DWITH_FFMPEG=OFF -DWITH_OPENCL=OFF -DWITH_QT=OFF -DWITH_IPP=OFF -DWITH_MATLAB=OFF -DWITH_OPENGL=OFF -DWITH_QT=OFF -DWITH_TIFF=ON -DWITH_TBB=OFF -DWITH_V4L=OFF -DWITH_LAPACK=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF WITH_OPENEXR=OFF ..
-cmake --build . --config Release --target ALL_BUILD
-cmake --build . --config Release --target INSTALL
+git checkout tags/4.3.0
+mkdir build & cd build
+cmake -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs,photo -DBUILD_opencv_apps=OFF -DBUILD_opencv_java_bindings_generator=OFF -DBUILD_opencv_python3=OFF -DBUILD_opencv_python_bindings_generator=OFF -DBUILD_opencv_python_tests=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_JAVA=OFF -DBUILD_JPEG=ON -DBUILD_IPP_IW=OFF -DBUILD_ITT=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_PNG=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_TIFF=ON -DBUILD_WEBP=OFF -DBUILD_ZLIB=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF -DWITH_FFMPEG=OFF -DWITH_IPP=OFF -DWITH_ITT=OFF -DWITH_JPEG=ON -DWITH_LAPACK=OFF -DWITH_MATLAB=OFF -DWITH_OPENCL=OFF -DWITH_OPENEXR=OFF -DWITH_OPENGL=OFF -DWITH_PNG=ON -DWITH_QT=OFF -DWITH_TBB=OFF -DWITH_TIFF=ON -DWITH_V4L=OFF -DWITH_WEBP=OFF ..
+cmake --build . --config %BUILD_TYPE% --target ALL_BUILD --parallel 4
+cmake --build . --config %BUILD_TYPE% --target INSTALL
+set OPENCV_INSTALL_DIR=%UCP_PATH%/deephealth/opencv/build
 
-:: ECVL
-cd %CUR_PATH%/deephealth
+REM ECVL
+cd %UCP_PATH%\deephealth
 git clone https://github.com/deephealthproject/ecvl.git
 cd ecvl
-git checkout ed0bc3dc90a5d93217f0d8d72e2d21b3aecc4925 
-mkdir build
-cd build
-cmake -G "Visual Studio 15 2017 Win64" -DOpenCV_DIR=%CUR_PATH%/deephealth/opencv/build -Deddl_DIR=%CUR_PATH%/deephealth/eddl/build/install -DECVL_BUILD_EDDL=ON -DECVL_DATASET_PARSER=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
-cmake --build . --config Release --target ALL_BUILD
-cmake --build . --config Release --target INSTALL
+git checkout tags/v0.2.0 
+mkdir build & cd build
+cmake -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOpenCV_DIR=%OPENCV_INSTALL_DIR% -Deddl_DIR=%EDDL_INSTALL_DIR% -DECVL_BUILD_EDDL=ON -DECVL_DATASET=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . --config %BUILD_TYPE% --target ALL_BUILD --parallel 4
+cmake --build . --config %BUILD_TYPE% --target INSTALL
+set ECVL_INSTALL_DIR=%UCP_PATH%/deephealth/ecvl/build/install
 
-:: PIPELINE
-cd %CUR_PATH%
-mkdir bin
-cd bin
-cmake -G "Visual Studio 15 2017 Win64" -Decvl_DIR=deephealth/ecvl/build/install ..
-cmake --build . --config Release --target ALL_BUILD
-Release\\MNIST_BATCH.exe
+REM PIPELINE
+cd %UCP_PATH%
+mkdir bin & cd bin
+cmake -A x64 -Decvl_DIR=%ECVL_INSTALL_DIR% ..
+cmake --build . --config %BUILD_TYPE% --target ALL_BUILD
+%BUILD_TYPE%\\MNIST_BATCH.exe
--- build_pipeline.sh
+++ build_pipeline.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
-CUR_PATH=`pwd`
+UCP_PATH=`pwd`
+DEVICE="CPU"
+BUILD_TYPE="Release"
+
 mkdir -p deephealth && cd deephealth
 
 ############ EDDL
 git clone --recurse-submodule https://github.com/deephealthproject/eddl.git 
 cd eddl
-git checkout tags/v0.4.2
+git checkout tags/0.4.3
 mkdir -p build && cd build
-cmake -G "Unix Makefiles" -DBUILD_TARGET=GPU -DBUILD_SHARED_LIB=OFF -DUSE_CUDA=ON -DCMAKE_INSTALL_PREFIX=install ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TARGET=$DEVICE -DBUILD_SHARED_LIB=OFF -DBUILD_PROTOBUF=OFF -DCMAKE_INSTALL_PREFIX=install ..
 make -j$(nproc) && make install
+EDDL_INSTALL_DIR=$UCP_PATH/deephealth/eddl/build/cmake
 
 ############ OPENCV
-cd $CUR_PATH/deephealth
+cd $UCP_PATH/deephealth
 git clone https://github.com/opencv/opencv.git
 cd opencv
-git checkout tags/4.1.1
+git checkout tags/4.3.0
 mkdir -p build && cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs,photo -DBUILD_opencv_apps=OFF -DBUILD_opencv_java_bindings_generator=OFF -DBUILD_opencv_python3=OFF -DBUILD_opencv_python_bindings_generator=OFF -DBUILD_opencv_python_tests=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_ZLIB=OFF -DBUILD_JPEG=ON -DBUILD_PNG=ON -DBUILD_TIFF=ON -DWITH_FFMPEG=OFF -DWITH_OPENCL=OFF -DWITH_QT=OFF -DWITH_IPP=OFF -DWITH_MATLAB=OFF -DWITH_OPENGL=OFF -DWITH_QT=OFF -DWITH_TIFF=ON -DWITH_TBB=OFF -DWITH_V4L=OFF -DWITH_LAPACK=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF WITH_OPENEXR=OFF ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=install -DBUILD_LIST=core,imgproc,imgcodecs,photo -DBUILD_opencv_apps=OFF -DBUILD_opencv_java_bindings_generator=OFF -DBUILD_opencv_python3=OFF -DBUILD_opencv_python_bindings_generator=OFF -DBUILD_opencv_python_tests=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_JAVA=OFF -DBUILD_JPEG=ON -DBUILD_IPP_IW=OFF -DBUILD_ITT=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_PNG=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_TIFF=ON -DBUILD_WEBP=OFF -DBUILD_ZLIB=OFF -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=OFF -DWITH_FFMPEG=OFF -DWITH_IPP=OFF -DWITH_ITT=OFF -DWITH_JPEG=ON -DWITH_LAPACK=OFF -DWITH_MATLAB=OFF -DWITH_OPENCL=OFF -DWITH_OPENEXR=OFF -DWITH_OPENGL=OFF -DWITH_PNG=ON -DWITH_QT=OFF -DWITH_TBB=OFF -DWITH_TIFF=ON -DWITH_V4L=OFF -DWITH_WEBP=OFF ..
 make -j$(nproc) && make install
+OPENCV_INSTALL_DIR=$UCP_PATH/deephealth/opencv/build
 
 ############ ECVL
-cd $CUR_PATH/deephealth
+cd $UCP_PATH/deephealth
 git clone https://github.com/deephealthproject/ecvl.git
 cd ecvl
-git checkout ed0bc3dc90a5d93217f0d8d72e2d21b3aecc4925  # Master branch
+git checkout tags/v0.2.0  # Release 0.2.0
 mkdir -p build && cd build
-cmake -G "Unix Makefiles" -DOpenCV_DIR=$CUR_PATH/deephealth/opencv/build -Deddl_DIR=$CUR_PATH/deephealth/eddl/build/cmake -DECVL_BUILD_EDDL=ON -DECVL_DATASET_PARSER=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DOpenCV_DIR=$OPENCV_INSTALL_DIR -Deddl_DIR=$EDDL_INSTALL_DIR -DECVL_BUILD_EDDL=ON -DECVL_DATASET=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
 make -j$(nproc) && make install
+ECVL_INSTALL_DIR=$UCP_PATH/deephealth/ecvl/build/install
 
 ############ PIPELINE
-cd $CUR_PATH
+cd $UCP_PATH
 mkdir -p bin && cd bin
-cmake -G "Unix Makefiles" -Decvl_DIR=$CUR_PATH/deephealth/ecvl/build/install ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -Decvl_DIR=$ECVL_INSTALL_DIR ..
 make -j$(nproc) && ./MNIST_BATCH
