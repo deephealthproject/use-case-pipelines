7796a4dc603d480eff2a49f48f7d9dcf1799bf1b
Laura Canalini <44258837+lauracanalini@users.noreply.github.com>
2020-05-15 10:54:26 +0200

    Update build_pipeline.bat

--- build_pipeline.bat
+++ build_pipeline.bat
@@ -37,7 +37,7 @@ git clone https://github.com/deephealthproject/ecvl.git
 cd ecvl
 git checkout tags/v0.2.1
 mkdir build & cd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOpenCV_DIR=%OPENCV_INSTALL_DIR% -Deddl_DIR=%EDDL_INSTALL_DIR% -DECVL_BUILD_EDDL=ON -DECVL_DATASET=ON -DECVL_BUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=install ..
+cmake -A x64 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DOpenCV_DIR=%OPENCV_INSTALL_DIR% -Deddl_DIR=%EDDL_INSTALL_DIR% -DECVL_BUILD_EDDL=ON -DECVL_DATASET=ON -DECVL_BUILD_GUI=OFF -DECVL_WITH_DICOM=ON -DCMAKE_INSTALL_PREFIX=install ..
 cmake --build . --config %BUILD_TYPE% --parallel %PROC%
 cmake --build . --config %BUILD_TYPE% --target INSTALL
 set ECVL_INSTALL_DIR=%UCP_PATH%/%DEPENDENCIES_DIR%/ecvl/build/install
