262cb7244e64fdd67c02a64ab2126608f29445bf
Laura Canalini <laura.canalini@unimore.it>
2020-05-19 11:17:18 +0200

    Update build_pipeline.sh and add install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+UCP_PATH=$(pwd)
+DEPENDENCIES_DIR="eddl_dependencies"
+
+mkdir -p $DEPENDENCIES_DIR && cd $DEPENDENCIES_DIR
+
+############ EDDL dependencies
+apt install zlib1g-dev 
+
+git clone https://gitlab.com/libeigen/eigen.git
+cd eigen
+git checkout tags/3.3.7
+mkdir -p build && cd build
+cmake ..
+make install
+
+cd $UCP_PATH/$DEPENDENCIES_DIR
+wget -q https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protobuf-cpp-3.11.4.tar.gz
+tar xf protobuf-cpp-3.11.4.tar.gz
+cd protobuf-3.11.4
+./configure
+make -j$(nproc)
+make install
+ldconfig
+
+cd $UCP_PATH
+rm -rf $DEPENDENCIES_DIR
